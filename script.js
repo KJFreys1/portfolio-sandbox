e9aa212dae8af97895aeefe36a12949a94ccce19
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-17 19:09:29 -0700

    added nav bar

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 const leftBtn = document.querySelector('.left')
 const rightBtn = document.querySelector('.right')
 const pages = document.querySelectorAll('.page')
-console.log(pages)
+const links = document.querySelectorAll('li')
 let pageIndex = 0
+
+function checkBtn(idx=pageIndex) {
+    rightBtn.style.opacity = idx < 3 ? 1 : .1
+    leftBtn.style.opacity = idx > 0 ? 1 : .1
+}
+
+links.forEach((li, idx) => {
+    li.addEventListener('click', () => {
+        let newPage = idx
+        checkBtn(newPage)
+        if (newPage > pageIndex) {
+            for (i = 0; i < newPage - pageIndex; i++) {
+                setTimeout(() => {
+                    pageIndex++
+                    pages[pageIndex].style.transform = 'translateX(0)'
+                }, i * 200)
+            }
+        } else if (newPage < pageIndex) {
+            for (i = 0; i < pageIndex - newPage; i++) {
+                setTimeout(() => {
+                    pages[pageIndex].style.transform = 'translateX(100vw)'
+                    pageIndex--
+                }, i * 200)
+            }
+        }
+    })
+})
 
 rightBtn.addEventListener('click', () => {
     if (pageIndex == 3) return
     pageIndex++
     pages[pageIndex].style.transform = 'translateX(0)'
-    if (pageIndex === 3) {
-        rightBtn.style.opacity = .1
-    } else if (pageIndex === 1) {
-        leftBtn.style.opacity = 1
-    }
+    checkBtn()
 })
 
 leftBtn.addEventListener('click', () => {
     if (pageIndex == 0) return
     pages[pageIndex].style.transform = 'translateX(100vw)'
     pageIndex--
-    if (pageIndex === 0) {
-        leftBtn.style.opacity = .1
-    } else if (pageIndex === 2) {
-        rightBtn.style.opacity = 1
-    }
+    checkBtn()
 })
