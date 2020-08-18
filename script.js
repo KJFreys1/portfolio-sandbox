02bb3341b11ce38641400b50701deb30a8d086f7
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-17 22:13:05 -0700

    header title animation

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const ui = document.querySelector('#ui')
 const leftBtn = document.querySelector('.left')
 const rightBtn = document.querySelector('.right')
 const pages = document.querySelectorAll('.page')
 const links = document.querySelectorAll('li')
 let pageIndex = 0
 
+const job = document.querySelector('.job')
+const jobTitles = ["Front End Developer", "Full Stack Engineer", "UI/UX Designer", "Major Tech Head"]
+let jobIndex = 0
+
 function checkBtn(idx=pageIndex) {
     rightBtn.style.opacity = idx < 3 ? 1 : .1
     leftBtn.style.opacity = idx > 0 ? 1 : .1
+    links.forEach((li, i) => {
+        if (i !== idx) {
+            li.classList.remove('highlight')
+        } else {
+            li.classList.add('highlight')
+        }
+    })
 }
+
+function changeJob() {
+    jobIndex = jobIndex < 3 ? jobIndex + 1 : 0
+    job.style.transform = 'translateY(10vh)'
+    setTimeout(() => {
+        job.textContent = jobTitles[jobIndex]
+    }, 800)
+    setTimeout(() => {
+        job.style.transform = 'translateY(0)'
+    }, 1200)
+    setTimeout(changeJob, 4000)
+}
+
+setTimeout(changeJob, 4000)
 
 links.forEach((li, idx) => {
     li.addEventListener('click', () => {
